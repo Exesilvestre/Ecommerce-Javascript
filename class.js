e977ad54ce9191bd1725bca7bf6b20f778f03ab9
Exesilvestre <106208814+Exesilvestre@users.noreply.github.com>
2022-08-01 12:00:33 -0300

    Delete class.js

--- class.js
+++
@@ -1,9 +0,0 @@
-class Producto {
-    constructor (cantidad, nombre, precio, tamaño, subtotalCompras){
-        this.cantidad = parseInt(cantidad)
-        this.nombre = nombre
-        this.precio = parseFloat(precio)
-        this.tamaño = tamaño
-        this.subtotal = subtotalCompras
-    }
-}
