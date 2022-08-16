87835cf573acb356106579a9829422f65ed91cf6
Exesilvestre <106208814+Exesilvestre@users.noreply.github.com>
2022-08-15 22:23:28 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const cambiarContraseña = document.getElementById("cambiarC");
-cambiarContraseña.addEventListener("click", contraseña);
-
-function contraseña(){
-    alert("No contamos con base de datos para almacenar y cambiar contraseñas");
-}
